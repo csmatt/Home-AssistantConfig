772d5752e2c9d422acfbcfd456aeaa7541e20f46
Matt Defenthaler <matt@csmatt.com>
2017-10-09 20:10:53 -0400

    Updated fan script, added zwave dimmers, and updated concord4 items

--- scripts/fan.py
+++ scripts/fan.py
@@ -13,13 +13,17 @@ PREAMBLE =  "101010101010101010101010"
 COMMAND_PREFIX = "01000101111001100110101111010000111110010111"
 
 COMMANDS = {
-  "lit1": "0111111110001000000001",
-  "lit2": "1110110010000001001101",
-  "fno1": "1111111100000000000010",
-  "fno2": "1110111010000001000100",
-  "fan1": "1111111010000000000100",
-  "fan2": "1110111110000001000001",
-  "fan3": "1101111110000010000001"
+  "light": [
+    "0111111110001000000001",
+    "1110110010000001001101"
+  ],
+  "fan_off": [
+    "1111111100000000000010",
+    "1110111010000001000100"
+  ],
+  "fan1": ["1111111010000000000100"],
+  "fan2": ["1110111110000001000001"],
+  "fan3": ["1101111110000010000001"]
 }
 
 def send_value(value):
@@ -29,17 +33,15 @@ def send_value(value):
 def send_0():
   send_value(GPIO.HIGH)
   send_value(GPIO.HIGH)
-  
+
 def send_1():
   send_value(GPIO.HIGH)
   send_value(GPIO.LOW)
-  
+
 def send_separator():
   send_value(GPIO.LOW)
 
-
-
-def do_it(choice):
+def send_command_part(part):
   for bit in PREAMBLE:
     if bit == "1":
       GPIO.output(TX_PIN, GPIO.HIGH)
@@ -51,7 +53,7 @@ def do_it(choice):
   GPIO.output(TX_PIN, GPIO.LOW)
   time.sleep(DELAY_AFTER_PREAMBLE)
 
-  rest = COMMAND_PREFIX + COMMANDS[choice]
+  rest = COMMAND_PREFIX + part
   for bit in rest:
     if bit == "0":
       send_0()
@@ -60,11 +62,6 @@ def do_it(choice):
     send_separator()
   time.sleep(DELAY_AFTER_COMMAND)
 
-def do_double_command(a, b):
-  do_it(a)
-  do_it(a)
-  do_it(b)
-  do_it(b)
 
 if __name__ == "__main__":
   # Pin Setup:
@@ -72,13 +69,8 @@ if __name__ == "__main__":
   GPIO.setup(TX_PIN, GPIO.OUT)
   try:
     choice = sys.argv[1]
-    if choice == "light":
-      do_double_command("lit1", "lit2")
-    elif choice == "fan_off":
-      do_double_command("fno1", "fno2")
-    else:
-      do_it(choice)
-      do_it(choice)
+    for part in COMMANDS[choice]:
+      send_command_part(part)
+      send_command_part(part)
   finally:
     GPIO.cleanup() # cleanup all GPIO
-
